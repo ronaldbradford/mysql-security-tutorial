b1885bd5971e83b89da494529d938ab1a91a5c45
Ronald Bradford <bradford@pythian.com>
2017-09-01 23:10:25 -0400

    First attempt to parameterize version

--- vagrant/centos7/community/install.sh
+++ vagrant/centos7/community/install.sh
@@ -1,4 +1,5 @@
 [ -z "${MYSQL_VERSION}" ] && MYSQL_VERSION="57"
+[ ! -z "$1" ] && MYSQL_VERSION="$1"
 sudo yum update -y
 sudo yum localinstall -y https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
 sudo yum-config-manager --disable mysql57-community
