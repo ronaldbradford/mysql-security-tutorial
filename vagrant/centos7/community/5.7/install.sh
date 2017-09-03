51ce9da696c0537f6f3ff477860553edc707ed6b
Ronald Bradford <bradford@pythian.com>
2017-09-03 15:02:11 -0400

    Revised Vagrantfile for specific hostname

---
+++ vagrant/centos7/community/5.7/install.sh
@@ -0,0 +1,13 @@
+[ -z "${MYSQL_VERSION}" ] && MYSQL_VERSION="57"
+[ ! -z "$1" ] && MYSQL_VERSION="$1"
+sudo yum update -y
+sudo yum localinstall -y https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+sudo yum-config-manager --disable mysql57-community
+sudo yum-config-manager --enable mysql${MYSQL_VERSION}-community
+yum repolist enabled | grep mysql
+sudo yum install -y mysql-community-server
+sudo systemctl start mysqld.service
+sudo systemctl enable mysqld.service
+
+echo "[client]
+prompt=`hostname` >" > ${HOME}/.my.cnf
