1922eca2b760acb20e5da01f1a39f86cc525f414
Ronald Bradford <bradford@pythian.com>
2017-09-04 14:40:55 -0400

    Added MySQL 8.0 vagrant setup

---
+++ vagrant/centos7/community/8.0/install.sh
@@ -0,0 +1,11 @@
+[ -z "${MYSQL_VERSION}" ] && MYSQL_VERSION="57"
+[ ! -z "$1" ] && MYSQL_VERSION="$1"
+MYSQL_VERSION=80
+sudo yum update -y
+sudo yum localinstall -y https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+sudo yum-config-manager --disable mysql57-community
+sudo yum-config-manager --enable mysql${MYSQL_VERSION}-community
+yum repolist enabled | grep mysql
+sudo yum install -y mysql-community-server
+sudo systemctl start mysqld.service
+sudo systemctl enable mysqld.service
