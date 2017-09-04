43efe3125d7dfccf69e716482a3f46f1f91e865a
Ronald Bradford <bradford@pythian.com>
2017-09-04 14:40:06 -0400

    Needed packages to install Mcafee audit plugin

--- vagrant/centos7/community/5.7/install.sh
+++ vagrant/centos7/community/5.7/install.sh
@@ -1,6 +1,7 @@
 [ -z "${MYSQL_VERSION}" ] && MYSQL_VERSION="57"
 [ ! -z "$1" ] && MYSQL_VERSION="$1"
 sudo yum update -y
+sudo yum install -y wget unzip
 sudo yum localinstall -y https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
 sudo yum-config-manager --disable mysql57-community
 sudo yum-config-manager --enable mysql${MYSQL_VERSION}-community
